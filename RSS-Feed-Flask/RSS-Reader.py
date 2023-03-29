6ad9148f708d383c69134ad2641a33e64a8bbac2
kosmik247 <kosmik247@gmail.com>
2023-03-29 09:37:15 +0100

    Base

---
+++ RSS-Feed-Flask/RSS-Reader.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = requests.get('https://feeds.bbci.co.uk/news/england/london/rss.xml')
+print(url)
+
+soup = BeautifulSoup(url.content, 'xml')
+entries = soup.find_all('item')
+
+for entry in entries:
+  title = entry.title.text
+  summary = entry.description.text
+  link = entry.link
+  print(f"Title: {title}\n\nSummary: {summary}\n\nLink: {link}\n\n------------------------\n\n")
