0b8115679fff6f76c2064aba98830fb231d709d1
Kosmik <89279935+Kosmik247@users.noreply.github.com>
2023-05-10 22:11:42 +0100

    Rewriting the parser method

--- RSS-Feed-Flask/RSS-Reader.py
+++ RSS-Feed-Flask/RSS-Reader.py
@@ -1,20 +1,15 @@
-from bs4 import BeautifulSoup
-import requests
-from website import db 
+import feedparser
+
+feedURL = 'http://feeds.bbci.co.uk/news/health/rss.xml'
 
 
+def print_news():
+  feed = feedparser.parse(feedURL)
+  article = feed['entries'][0]
+  
+  for article in feed['entries']:
+    print(article.get("title"))
+    print(article.get("description"))
+    print(article.get("link"))
 
-
-
-def get_rss(url):
-  url = requests.get('https://feeds.bbci.co.uk/news/england/london/rss.xml')
-  soup = BeautifulSoup(url.content, 'xml')
-  entries = soup.find_all('item')
-
-  for entry in entries:
-    title = entry.title.text
-    summary = entry.description.text
-    link = entry.link
-    print(f"Title: {title}\n\nSummary: {summary}\n\nLink: {link}\n\n------------------------\n\n")
-
-get_rss()
+print_news()
