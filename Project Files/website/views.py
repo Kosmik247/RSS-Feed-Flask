3c19ef3f3abfe9e8175c97fe6234b63a8e2b0415
Kosmik <89279935+Kosmik247@users.noreply.github.com>
2023-09-02 12:04:57 +0100

    Sidebar - Website adding separate screen #Prototype 2 (THIS ACTUALLY WORKS)

--- Project Files/website/views.py
+++ Project Files/website/views.py
@@ -1,24 +1,35 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 import feedparser
 from . import db
 from .db_models import RSS_Data
 views = Blueprint('views', __name__)
 
-
-@views.route('/', methods=['GET', 'POST'])
+# Could use Jsonify as an alternative packaging tool for the returned information.
+@views.route('/', methods=['GET','POST'])
 @login_required
 def home():
-    feedURL = 'http://feeds.bbci.co.uk/news/health/rss.xml'
-    feed = feedparser.parse(feedURL)
+    website_link = 'None'
 
+    if request.method == 'POST':
+        website_title = request.form.get('feed_id')
 
+        website = RSS_Data.query.filter_by(title=website_title).first()
+        if website:
+            website_link = website.link
+            print(website_link)
+
+        else:
+            print("No websites stored in database.")
+            website_link="None"
+
+    feed = feedparser.parse(website_link)
     for article in feed['entries']:
         print(article.get("title"))
         print(article.get("description"))
         print(article.get("link"))
 
-    return render_template("home.html", user=current_user, feeds=feed['entries'], url=feedURL)
+    return render_template("home.html", user=current_user, feeds=feed['entries'])
 
 
 @views.route('/profile')
@@ -26,10 +37,16 @@ def home():
 def profile():
     return render_template("profile.html", user=current_user)
 
-@views.route('/add_website', methods=['POST'])
+@views.route('/add_links', methods=['GET', 'POST'])
 @login_required
-def add_website():
-    website_data = RSS_Data(link=request.form['website'], user_id=current_user.id)
-    db.session.add(website_data)
-    db.session.commit()
-    return render_template("profile.html", user=current_user)
+def add_links():
+    if request.method == 'POST':
+        website_title = request.form.get('web_title')
+        website_link = request.form.get('web_link')
+        new_link = RSS_Data(title=website_title,link=website_link,user_id=current_user.id)
+        db.session.add(new_link)
+        db.session.commit()
+
+    return render_template("website_add.html", user=current_user)
+
+
