28e6f785f154edd819579d64df5676b6b49e010d
Kosmik <89279935+Kosmik247@users.noreply.github.com>
2024-03-10 20:29:58 +0000

    Final Refactoring, changing test data

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 # Main file run
 # Module Imports
-from website import create_app # Website Module
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+
+from website import create_app  # Website Module
 from website import db_models, db
 from website.admin_dashboard_auth import Admin_View
-
 
 # Initialises the app function in the website module
 app = create_app()
 # Initialises the different interface for the admin screen
 admin = Admin(app, index_view=Admin_View())
 admin.add_view(ModelView(db_models.User, db.session))
-admin.add_view(ModelView(db_models.RSS_Data,db.session))
-admin.add_view(ModelView(db_models.Tags,db.session))
+admin.add_view(ModelView(db_models.RSS_Data, db.session))
+admin.add_view(ModelView(db_models.Tags, db.session))
 
-#Runs file name __main__
+# Runs file name __main__
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=8001)
-
